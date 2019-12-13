163508b29b8266649bb8e464e2d61d7d857696a2
Fernando Boucquez <fboucquez@gmail.com>
2019-12-13 09:42:19 -0300

    Improved javadoc

--- sdk-core/src/main/java/io/nem/sdk/api/MosaicRestrictionTransactionService.java
+++ sdk-core/src/main/java/io/nem/sdk/api/MosaicRestrictionTransactionService.java
@@ -30,10 +30,19 @@ import java.math.BigInteger;
 public interface MosaicRestrictionTransactionService {
 
     /**
-     * Create a {@link MosaicGlobalRestrictionTransactionFactory} object without previous
-     * restriction data
+     * Create a {@link MosaicGlobalRestrictionTransactionFactory} object that can be used to create
+     * or update a mosaic global restriction.
      *
-     * @param mosaicId MosaicId
+     * This service checks the current global restriction value using rest and set the corresponding
+     * previousRestrictionType and previousRestrictionValue so an update can be performed.
+     *
+     *
+     * When using aliases, the service will resolve them in order to query the mosaic restriction
+     * rest endpoint. This could add some overhead. If you know the real mosaic id, it's recommended
+     * to use it.
+     *
+     * @param mosaicId the mosaic id or an alias. If an alias is sent, the service will resolve it
+     * in order to retrieve the current global restriction value.
      * @param restrictionKey Restriction key
      * @param restrictionValue New restriction value
      * @param restrictionType New restriction type
@@ -47,12 +56,21 @@ public interface MosaicRestrictionTransactionService {
 
 
     /**
-     * Create a {@link MosaicAddressRestrictionTransactionFactory} object without previous
-     * restriction data
+     * Create a {@link MosaicAddressRestrictionTransactionFactory} object that can be used to create
+     * or update a mosaic address restriction.
      *
-     * @param mosaicId MosaicId
+     * This service checks the current address restriction value using rest and set the
+     * corresponding previousRestrictionValue so an update can be performed.
+     *
+     * When using aliases, the service will resolve them in order to query the mosaic restriction
+     * rest endpoint. This could add some overhead. If you know the real mosaic id and target
+     * address, it's recommended to use them.
+     *
+     * @param mosaicId the mosaic id or an alias. If an alias is sent, the service will resolve it *
+     * in order to retrieve the current mosaic address restriction value.
      * @param restrictionKey Restriction key
-     * @param targetAddress Target address
+     * @param targetAddress the target address or an alias. If an alias is sent, the service will
+     * resolve it in order to retrieve the current mosaic address restriction value.
      * @param restrictionValue New restriction value
      * @return {@link MosaicAddressRestrictionTransactionFactory} object without previous
      * restriction data
