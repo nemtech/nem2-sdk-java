380972c965b9f1a5890139dbe3201f4ea6a184ca
fernando <fboucquez@gmail.com>
2020-10-20 12:29:21 -0300

    Currency Service Improved Currency service to load the mosaics based on the network properties and not the the block 1 TransactionServiceTest Updated bootstrap to alpha Changed travis to run e2e on cron job over the main branch

--- e2e.sh
+++ e2e.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$TRAVIS" = "true" ]; then
     echo "Installing node"
     nvm install v12.18.3
-    npm install -g symbol-bootstrap
+    npm install -g symbol-bootstrap@alpha
 fi
 source bootstrap-start.sh -d
 gradle integrationTest
