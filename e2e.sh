cbc7782a3316f81114b370f08a29b34c8d3eb993
fernando <fboucquez@gmail.com>
2020-10-29 13:21:28 -0300

    FinalizationRepository Updated transaction search and hash lock search

--- e2e.sh
+++ e2e.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-
 if [ "$TRAVIS" = "true" ]; then
     echo "Installing node"
     nvm install v12.18.3
-    npm install -g symbol-bootstrap@0.2.0
+    npm install -g symbol-bootstrap@0.2.1-alpha-202010281951
 fi
 source bootstrap-start.sh -d
 gradle integrationTest
